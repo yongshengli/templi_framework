deb453ad14072039c4ad73061d41060c5e3667bc
七觞酒 <739800600@qq.com>
2014-06-25 10:56:21 +0800

    修改 cache_memcache 类

--- Cache/Cache_memcache.class.php
+++ Cache/Cache_memcache.class.php
@@ -6,29 +6,31 @@ defined('IN_TEMPLI') or die('非法引用');
  * @email 739800600@qq.com
  * @date  2013-3-22
  */
-class Cache_memcahe{
-    private $memcache;
+class Cache_memcahe extends Memcache
+{
     public function __construct(){
-        $this->memcache = new Memcache;
-		$this->memcache->connect(templi::get_config('cache_host'), templi::get_config('cache_port'), templi::get_config('cache_timeout'));
+		$this->connect(templi::get_config('cache_host'), templi::get_config('cache_port'), templi::get_config('cache_timeout'));
     }
     /**
      * 获取缓存
      * @param string $name  memcache id
      * @param boole
      */
-    public function get($name){
-        $value = $this->memcache->get($name);
+    public function get($name, $flag=0){
+        $value = parent::get($name, $flag);
 		return $value;
     }
     /**
      * 设置缓存
+     *
      * @param sting $name memecache id
-     * @param string $value 缓存值
+     * @param mixed $value 缓存值
+     * @param int expire 有效期
+     * @param bool $flage 是否压缩
      * @param boole
      */
-    public function set($name, $value, $ttl = 0, $ext1=null, $ext2=null){
-        return $this->memcache->set($name, $value, false, $ttl);
+    public function set($name, $value, $expire = 0, $flag=false){
+        return parent::set($name, $value, $flag, $expire);
     }
     /**
      * 清除缓存
@@ -37,17 +39,9 @@ class Cache_memcahe{
      */
     public function clean($name=null){
         if($name){
-            return $this->memcache->delete($name);
+            return $this->delete($name);
         }else{
-            return $this->memcache->flush();
+            return $this->flush();
         }
-        
-    }
-    /**
-     * 关闭连接
-     */
-    public function close(){
-        $this->memcache->close();
     }
 }
-?>
